b9761070a737a32ff153424f789348e7f71aa858
EllmanT <tmuranda1@gmail.com>
2026-06-01 19:28:01 +0200

    Refactor Footer component text for clarity and comment out unused social media links in data

--- data/index.ts
+++ data/index.ts
@@ -217,15 +217,15 @@ export const workExperience = [
 ];
 
 export const socialMedia = [
-  {
-    id: 1,
-    img: "/git.svg",
-    link: "https://github.com/EllmanT",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
+  // {
+  //   id: 1,
+  //   img: "/git.svg",
+  //   link: "https://github.com/EllmanT",
+  // },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  // },
   {
     id: 3,
     img: "/link.svg",
